2c461c48f7d3b20841eeea2d2d97ff326252195d
u8slvn <u8slvn@gmail.com>
2018-03-28 00:54:44 +0200

    refactor: work on structure

--- rer_watcher.py
+++ rer_watcher.py
@@ -1,86 +1,119 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# coding: utf-8
+
+import time
+from configparser import RawConfigParser
+from datetime import datetime
 
 import requests
-import time
-
-from requests.auth import HTTPBasicAuth
-from lxml import etree
-from datetime import datetime
-# from luma.led_matrix.device import max7219
 # from luma.core.interface.serial import spi, noop
-# from luma.core.render import canvas
 # from luma.core.legacy import text
 # from luma.core.legacy.font import proportional, LCD_FONT
+# from luma.core.render import canvas
+# from luma.led_matrix.device import max7219
+from lxml import etree
+from requests.auth import HTTPBasicAuth
 
-# Basic fetch time config
-DEFAULT_WAITING_TIME = 10
-STEP_WAITING_TIME = 10
-MAX_WAITING_TIME = 30
 
-# Api credentials
-URL_API = 'http://api.transilien.com/gare/87543207/depart/87547315'
-USER = 'user'
-PASSWORD = 'password'
+def timedelta_formatter(time_value):
+    hours = time_value.seconds // 3600
+    minutes = (time_value.seconds // 60) % 60
+    if minutes < 1:
+        minutes = 1
 
-def message_formatter(miss, date):
-    date_train = datetime.strptime(date, '%d/%m/%Y %H:%M')
-    date = int( round( (date_train - datetime.now()).seconds / 60 ) )
-    date = "%smin" % str(date) if date < 60 else "%sh" % str(date//60)
+    str = "{}min".format(minutes)
+    if hours > 1:
+        str = "{}h".format(hours)
 
-    return ("%s : %s" % (miss, date))
+    return str
 
-def manage_waiting_time(is_error, waiting_time):
-    if is_error:
-        if waiting_time < MAX_WAITING_TIME:
-            waiting_time += STEP_WAITING_TIME
-        return waiting_time
 
-    return DEFAULT_WAITING_TIME
-
-def rer_watcher():
-    # create matrix device
+def matrix_device_builder():
     # serial = spi(port=0, device=0, gpio=noop())
     # device = max7219(serial, width=64, height=16, block_orientation=-90, rotate=0)
-    # device.contrast(32)
-    # print("Matrix device created")
+    # return device.contrast(32)
+    return True
 
-    waiting_time = DEFAULT_WAITING_TIME
-    messages = []
 
-    while True:
-        try:
-            r = requests.get(URL_API, auth=HTTPBasicAuth(USER, PASSWORD))
+def bootstrap():
+    config = RawConfigParser()
+    config.read('settings.ini')
 
-            tree = etree.fromstring(r.text.encode('utf-8'))
+    matrix_device = matrix_device_builder()
 
-            for train in tree.xpath('/passages/train')[:2]:
-                msg = message_formatter(
-                    train.find('miss').text,
-                    train.find('date').text
-                )
-                messages.append(msg)
-            messages.reverse()
+    app = RERWatcher(
+        config=config,
+        display_device=matrix_device
+    )
+    app.start()
 
-            waiting_time = manage_waiting_time(False, waiting_time)
-        except requests.exceptions.RequestException as e:
-            waiting_time = manage_waiting_time(True, waiting_time)
-            messages.append('Connexion error')
-        except Exception as e:
-            waiting_time = manage_waiting_time(True, waiting_time)
-            messages.append('Data error')
-        finally:
-            for i in range(len(messages)):
-                print(messages.pop(i - 1))
 
-            # with canvas(device) as draw:
-            #     for msg in messages:
-            #         text(draw, (0, 9), "World", fill="white", font=proportional(LCD_FONT))
+class RERWatcher:
+    def __init__(self, config, display_device):
+        self.is_running = False
+        self._config = config
+        self._api_date_format = self._config.get('API', 'DateFormat')
+        self._refresh_time = self._config.getint('REFRESH_TIME', 'Default')
+        self._step_refresh_time = self._config.getint('REFRESH_TIME', 'Step')
+        self._max_refresh_time = self._config.getint('REFRESH_TIME', 'Max')
+        self._display_device = display_device
 
-            time.sleep(waiting_time);
+    def start(self):
+        self.is_running = True
+
+        while self.is_running:
+            api_result = self.fetch_api()
+            timetables = self.extract_timetables(api_load=api_result)
+            self.display_timetables(timetables=timetables)
+            self.manage_refresh_time()
+
+    def fetch_api(self):
+        return requests.get(
+            url=self._config.get('API', 'Url'),
+            auth=HTTPBasicAuth(
+                username=self._config.get('API', 'User'),
+                password=self._config.get('API', 'Password')
+            )
+        )
+
+    def extract_timetables(self, api_load, limit=2):
+        timetables_list = []
+        tree = etree.fromstring(api_load.text.encode('utf-8'))
+
+        for train in tree.xpath('/passages/train')[:limit]:
+            formatted_timetables = self.timetable_formatter(
+                miss=train.find('miss').text,
+                date=train.find('date').text
+            )
+            timetables_list.append(formatted_timetables)
+        timetables_list.reverse()
+
+        return timetables_list
+
+    def display_timetables(self, timetables):
+        # with canvas(self._display_device) as draw:
+        #     for msg in messages:
+        #         text(draw, (0, 9), "World", fill="white", font=proportional(LCD_FONT))
+        for timetable in timetables:
+            print(timetable)
+
+    def manage_refresh_time(self):
+        time.sleep(self._refresh_time)
+
+    def increase_refresh_time(self):
+        if self._refresh_time < self._max_refresh_time:
+            self._refresh_time += self._step_refresh_time
+
+    def timetable_formatter(self, miss, date):
+        date_train = datetime.strptime(date, self._api_date_format)
+        diff = date_train - datetime.now()
+        date = timedelta_formatter(diff)
+
+        return ("{miss}: {date}".format(miss=miss, date=date))
+
 
 if __name__ == "__main__":
     try:
-        rer_watcher()
+        bootstrap()
     except KeyboardInterrupt:
         pass
--- test_rer_watcher.py
+++ test_rer_watcher.py
@@ -1,26 +1,76 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# coding: utf-8
 
-from freezegun import freeze_time
+from configparser import RawConfigParser
+from datetime import timedelta
+from unittest.mock import patch, Mock
+
 import pytest
+from freezegun import freeze_time
+
 import rer_watcher
 
-@freeze_time("27-10-2017 13:31")
-@pytest.mark.parametrize("miss,date,expected", [
-        ('ROMI', '27/10/2017 13:41', 'ROMI : 10min'),
-        ('MONA', '27/10/2017 21:41', 'MONA : 8h'),
-    ])
-def test_message_formatter(miss, date, expected):
-    assert rer_watcher.message_formatter(miss, date) == expected
 
-@pytest.mark.parametrize("is_error,waiting_time,expected", [
-        (False, 10, 10),
-        (True, 10, 20),
-        (True, 20, 30),
-        (True, 30, 30),
-    ])
-def test_manage_waiting_time(is_error, waiting_time, expected):
-    rer_watcher.DEFAULT_WAITING_TIME = 10
-    rer_watcher.STEP_WAITING_TIME = 10
-    rer_watcher.MAX_WAITING_TIME = 30
-    assert rer_watcher.manage_waiting_time(is_error, waiting_time) == expected
+@pytest.mark.parametrize("given,expected", [
+    (timedelta(seconds=40), '1min',),
+    (timedelta(seconds=120), '2min',),
+    (timedelta(seconds=120), '2min',),
+])
+def test_timedelta_formatter(given, expected):
+    assert rer_watcher.timedelta_formatter(given) == expected
+
+
+@patch('rer_watcher.RERWatcher')
+@patch('rer_watcher.matrix_device_builder')
+@patch('rer_watcher.RawConfigParser.read')
+def test_bootstrap_should_create_an_app(rawconfigparser_mock,
+                                        device_builder_mock,
+                                        rer_watcher_mock):
+    # GIVEN
+    device_builder_mock.return_value = 'FOO-DEVICE-BUILDER'
+
+    # WHEN
+    rer_watcher.bootstrap()
+
+    # THENself.app
+    expected_config = RawConfigParser()
+    rer_watcher_mock.assert_called_with(config=expected_config,
+                                        display_device='FOO-DEVICE-BUILDER')
+
+
+@freeze_time("27-10-2018 13:30")
+class TestRERWatcher:
+    def setup(self):
+        mock = Mock()
+        self.app = rer_watcher.RERWatcher(mock, mock)
+        self.app._api_date_format = '%d/%m/%Y %H:%M'
+
+    @patch('rer_watcher.timedelta_formatter')
+    def test_timetable_formatter_should_return_str(self,
+                                                   timedelta_formatter_mock):
+        # GIVEN
+        miss = 'FOO'
+        date = '27/10/2018 13:39'
+        timedelta_formatter_mock.return_value = '9min'
+
+        # WHEN
+        result = self.app.timetable_formatter(miss=miss, date=date)
+
+        # THEN
+        assert 'FOO: 9min' == result
+
+    @patch('rer_watcher.timedelta_formatter')
+    def test_timetable_formatter_should_call_timedelta_formatter(
+            self,
+            timedelta_formatter_mock
+    ):
+        # GIVEN
+        miss = 'FOO'
+        date = '27/10/2018 13:31'
+
+        # WHEN
+        self.app.timetable_formatter(miss=miss, date=date)
+
+        # THEN
+        expected_timedelta = timedelta(seconds=60)
+        timedelta_formatter_mock.assert_called_with(expected_timedelta)
