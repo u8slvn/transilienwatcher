9be93187d6c38ba44badfca4acdd379a4defd108
u8slvn <u8slvn@gmail.com>
2018-03-30 01:28:25 +0200

    refactor: split code into more domain classes

--- rerwatcher/app.py
+++ rerwatcher/app.py
@@ -15,23 +15,6 @@ from lxml import etree
 from requests.auth import HTTPBasicAuth
 
 
-def timedelta_formatter(time_value):
-    hours = time_value.seconds // 3600
-    minutes = (time_value.seconds // 60) % 60
-    if minutes < 1:
-        minutes = 1
-
-    str = "{}min".format(minutes)
-    if hours > 1:
-        str = "{}h".format(hours)
-
-    return str
-
-
-def get_text_response_encoded(response, encoding='utf-8'):
-        return response.text.encode(encoding)
-
-
 def matrix_device_builder():
     # serial = spi(port=0, device=0, gpio=noop())
     # device = max7219(serial, width=64, height=16, block_orientation=-90, rotate=0)
@@ -39,31 +22,40 @@ def matrix_device_builder():
     return True
 
 
-def bootstrap():
+def load_config():
     config = RawConfigParser()
     config.read('settings.ini')
 
+    return config
+
+
+def bootstrap():
+    config = load_config()
+
+    api_driver = TransilienApiDriver(config)
+
     matrix_device = matrix_device_builder()
 
-    app = RERWatcher(
+    app = RerWatcher(
         config=config,
+        api_driver=api_driver,
         display_device=matrix_device
     )
     app.start()
 
 
-class RERWatcher:
-    def __init__(self, config, display_device):
+class RerWatcher:
+    def __init__(self, config, api_driver, display_device):
         self.is_running = False
-        self._api_url = config.get('API', 'Url')
-        self._api_date_format = config.get('API', 'DateFormat')
+        self._api_url = config.get('api', 'url')
         self._api_auth = HTTPBasicAuth(
-            username=config.get('API', 'User'),
-            password=config.get('API', 'Password')
+            username=config.get('api', 'user'),
+            password=config.get('api', 'password')
         )
-        self._refresh_time = config.getint('REFRESH_TIME', 'Default')
-        self._step_refresh_time = config.getint('REFRESH_TIME', 'Step')
-        self._max_refresh_time = config.getint('REFRESH_TIME', 'Max')
+        self._refresh_time = config.getint('refresh_time', 'default')
+        self._refresh_time_step = config.getint('refresh_time', 'step')
+        self._refresh_time_max = config.getint('refresh_time', 'max')
+        self.api_driver = api_driver
         self._display_device = display_device
 
     def start(self):
@@ -71,50 +63,82 @@ class RERWatcher:
 
         # TODO - manage context error
         while self.is_running:
-            api_response = self.fetch_api()
+            response = self._fetch_api()
             # TODO - check status_code == 200
-            api_load = get_text_response_encoded(response=api_response)
-            timetables = self.extract_timetables(api_load=api_load)
-            self.display_timetables(timetables=timetables)
-            self.manage_refresh_time()
+            timetables = self.api_driver.get_timetables(response=response)
+            self._display_timetables(timetables=timetables)
+            self._manage_refresh_time()
 
-    def fetch_api(self):
+    def _fetch_api(self):
         return requests.get(url=self._api_url, auth=self._api_auth)
 
-    def extract_timetables(self, api_load, limit=2):
-        timetables_list = []
-        tree = etree.fromstring(api_load)
-        trains = tree.xpath('/passages/train')
-
-        for train in trains[:limit]:
-            formatted_timetables = self.timetable_formatter(
-                miss=train.find('miss').text,
-                date=train.find('date').text
-            )
-            timetables_list.append(formatted_timetables)
-
-        return timetables_list
-
-    def display_timetables(self, timetables):
+    def _display_timetables(self, timetables):
         # with canvas(self._display_device) as draw:
         #     for msg in messages:
         #         text(draw, (0, 9), "World", fill="white", font=proportional(LCD_FONT))
         for timetable in timetables:
-            print(timetable)
+            print(timetable.display())
 
-    def manage_refresh_time(self):
+    def _manage_refresh_time(self):
         time.sleep(self._refresh_time)
 
-    def increase_refresh_time(self):
-        if self._refresh_time < self._max_refresh_time:
-            self._refresh_time += self._step_refresh_time
+    def _increase_refresh_time(self):
+        if self._refresh_time < self._refresh_time_max:
+            self._refresh_time += self._refresh_time_step
 
-    def timetable_formatter(self, miss, date):
-        date_train = datetime.strptime(date, self._api_date_format)
-        diff = date_train - datetime.now()
-        date = timedelta_formatter(diff)
 
-        return ("{miss}: {date}".format(miss=miss, date=date))
+class TransilienApiDriver:
+    def __init__(self, config):
+        self._date_format = config.get('api', 'date_format')
+        self._encoding = config.get('api', 'encoding')
+
+    def get_timetables(self, response, limit=2):
+        response_body = response.text.encode(self._encoding)
+
+        tree = etree.fromstring(response_body)
+        trains = tree.xpath('/passages/train')
+
+        timetables_list = list()
+
+        for train in trains[:limit]:
+            miss = train.find('miss').text
+            date = train.find('date').text
+
+            date = self._convert_date_to_time(date)
+
+            timetable = TimeTable(miss=miss, date=date)
+
+            timetables_list.append(timetable)
+
+        return timetables_list
+
+    def _convert_date_to_time(self, date_str):
+        date = datetime.strptime(date_str, self._date_format)
+        diff = date - datetime.now()
+        time = self._timedelta_formatter(diff)
+
+        return time
+
+    def _timedelta_formatter(self, time_value):
+        hours = time_value.seconds // 3600
+        minutes = (time_value.seconds // 60) % 60
+        if minutes < 1:
+            minutes = 1
+
+        time_str = "{}min".format(minutes)
+        if hours > 1:
+            time_str = "{}h".format(hours)
+
+        return time_str
+
+
+class TimeTable:
+    def __init__(self, miss, date):
+        self.miss = miss
+        self.date = date
+
+    def display(self):
+        return ("{miss}: {date}".format(miss=self.miss, date=self.date))
 
 
 if __name__ == "__main__":
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-from configparser import RawConfigParser
 from datetime import timedelta
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, MagicMock
 
 import pytest
 from freezegun import freeze_time
@@ -11,82 +10,58 @@ from freezegun import freeze_time
 from rerwatcher import app
 
 
-@pytest.mark.parametrize("given,expected", [
-    (timedelta(seconds=40), '1min',),
-    (timedelta(seconds=120), '2min',),
-    (timedelta(seconds=7400), '2h',),
-])
-def test_timedelta_formatter(given, expected):
-    assert app.timedelta_formatter(given) == expected
+@patch('rerwatcher.app.RawConfigParser')
+def test_load_config_return_data(rawconfigparser_mock):
+    # GIVEN
+    config_mock = MagicMock()
+    config_mock.configure_mock(name='FOO-CONFIG')
+    rawconfigparser_mock.return_value = config_mock
+
+    # WHEN
+    config = app.load_config()
+
+    # THEN
+    assert config.name == 'FOO-CONFIG'
 
 
-@patch('rerwatcher.app.RERWatcher')
+@patch('rerwatcher.app.RerWatcher')
 @patch('rerwatcher.app.matrix_device_builder')
-@patch('rerwatcher.app.RawConfigParser.read')
-def test_bootstrap_should_create_an_app(rawconfigparser_mock,
+@patch('rerwatcher.app.TransilienApiDriver')
+@patch('rerwatcher.app.load_config')
+def test_bootstrap_should_create_an_app(load_config_mock,
+                                        api_driver_mock,
                                         device_builder_mock,
                                         app_mock):
     # GIVEN
+    load_config_mock.return_value = 'FOO-CONFIG'
+    api_driver_mock.return_value = 'FOO-API-DRIVER'
     device_builder_mock.return_value = 'FOO-DEVICE-BUILDER'
 
     # WHEN
     app.bootstrap()
 
-    # THENself.app
-    expected_config = RawConfigParser()
-    app_mock.assert_called_with(config=expected_config,
+    # THEN
+    app_mock.assert_called_with(config='FOO-CONFIG',
+                                api_driver='FOO-API-DRIVER',
                                 display_device='FOO-DEVICE-BUILDER')
 
 
-@freeze_time("27-10-2018 13:30")
-class TestRERWatcher:
+class TestRerWatcher:
     def setup(self):
-        mock = Mock()
-        self.app = app.RERWatcher(mock, mock)
-        self.app._api_date_format = '%d/%m/%Y %H:%M'
-
-    @patch('rerwatcher.app.timedelta_formatter')
-    def test_timetable_formatter_should_return_str(self,
-                                                   timedelta_formatter_mock):
-        # GIVEN
-        miss = 'FOO'
-        date = '27/10/2018 13:39'
-        timedelta_formatter_mock.return_value = '9min'
-
-        # WHEN
-        result_timetale = self.app.timetable_formatter(miss=miss, date=date)
-
-        # THEN
-        expected_timetable = 'FOO: 9min'
-        assert result_timetale == expected_timetable
-
-    @patch('rerwatcher.app.timedelta_formatter')
-    def test_timetable_formatter_should_call_timedelta_formatter(
-            self,
-            timedelta_formatter_mock
-    ):
-        # GIVEN
-        miss = 'FOO'
-        date = '27/10/2018 13:31'
-
-        # WHEN
-        self.app.timetable_formatter(miss=miss, date=date)
-
-        # THEN
-        expected_timedelta = timedelta(seconds=60)
-        timedelta_formatter_mock.assert_called_with(expected_timedelta)
+        self.app = app.RerWatcher(
+            config=Mock(), api_driver=Mock(), display_device=Mock()
+        )
 
     @patch('rerwatcher.app.requests.get')
-    def test_fetch_api_sould_return_data(self, requests_mock):
+    def test_fetch_api_should_return_data(self, requests_mock):
         # GIVEN
         requests_mock.return_value = 'FOO-DATA'
 
         # WHEN
-        result_data = self.app.fetch_api()
+        data = self.app._fetch_api()
 
         # THEN
-        expected_data = 'FOO-DATA'
-        assert result_data == expected_data
+        assert data == 'FOO-DATA'
 
     @patch('rerwatcher.app.time.sleep')
     def test_manage_refresh_time_should_call_sleep(self, time_mock):
@@ -94,40 +69,89 @@ class TestRERWatcher:
         self.app._refresh_time = 10
 
         # WHEN
-        self.app.manage_refresh_time()
+        self.app._manage_refresh_time()
 
         # THEN
         time_mock.assert_called_with(10)
 
-    def test_get_response_text_encoded_should_return_data(self):
+    @pytest.mark.parametrize('times,expected', [
+        (1, 20),
+        (2, 30),
+        (3, 30),
+    ])
+    def test_increase_refresh_time(self, times, expected):
         # GIVEN
-        data_mock = Mock()
-        data_mock.text.encode.return_value = 'FOO-DATA'
+        self.app._refresh_time = 10
+        self.app._refresh_time_max = 30
+        self.app._refresh_time_step = 10
 
         # WHEN
-        result_data = app.get_text_response_encoded(data_mock)
+        for _ in range(times):
+            self.app._increase_refresh_time()
 
         # THEN
-        expected_data = 'FOO-DATA'
-        assert result_data == expected_data
+        assert self.app._refresh_time is expected
 
-    @patch('rerwatcher.app.RERWatcher.timetable_formatter')
+
+class TestTransilienApiDriver:
+    def setup(self):
+        self.api_driver = app.TransilienApiDriver(Mock())
+        self.api_driver._date_format = '%d/%m/%Y %H:%M'
+        self.api_driver._encoding = 'utf-8'
+
+    @pytest.mark.parametrize("given,expected", [
+        (timedelta(seconds=40), '1min',),
+        (timedelta(seconds=120), '2min',),
+        (timedelta(seconds=7400), '2h',),
+    ])
+    def test_timedelta_formatter(self, given, expected):
+        assert self.api_driver._timedelta_formatter(given) == expected
+
+    @freeze_time("27-10-2018 13:30")
+    def test_convert_date_to_time(self):
+        # GIVEN
+        api_driver = self.api_driver
+        api_driver._timedelta_formatter = Mock(return_value='2min')
+        date_str = '27/10/2018 13:32'
+
+        # WHEN
+        time = api_driver._convert_date_to_time(date_str=date_str)
+
+        # THEN
+        assert time == '2min'
+        api_driver._timedelta_formatter.assert_called_once_with(
+            timedelta(seconds=120))
+
+    @patch('rerwatcher.app.TimeTable')
     @patch('rerwatcher.app.etree')
     def test_extract_timetables_should_return_two_elements(
             self,
             etree_mock,
-            timetable_formatter_mock
+            timetable_mock
     ):
         # GIVEN
+        api_driver = self.api_driver
+        api_driver._convert_date_to_time = Mock()
         tree_mock = Mock()
-        tree_mock.xpath.return_value = [Mock(name=1), Mock(name=2)]
+        tree_mock.xpath.return_value = [Mock(), Mock()]
         etree_mock.fromstring.return_value = tree_mock
-        timetable_formatter_mock.side_effect = ['FOO-ONE', 'FOO-TWO']
+        timetable_mock.side_effect = ['FOO', 'BAR']
 
         # WHEN
-        result_list = self.app.extract_timetables(None)
+        result_list = api_driver.get_timetables(Mock())
 
         # THEN
-        expected_list = ['FOO-ONE', 'FOO-TWO']
-        assert result_list == expected_list
-        assert len(timetable_formatter_mock.call_args_list) is 2
+        assert result_list == ['FOO', 'BAR']
+        assert len(api_driver._convert_date_to_time.call_args_list) is 2
+
+
+class TestTimeTable:
+    def test_display(self):
+        # GIVEN
+        timetable = app.TimeTable('FOO', 'BAR')
+
+        # WHEN
+        message = timetable.display()
+
+        # THEN
+        assert message == 'FOO: BAR'
